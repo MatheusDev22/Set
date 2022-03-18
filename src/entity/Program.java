b77982b677db86a57c1f70fc98ff092a04ca11fd
MatheusDev22 <matheusbsb95@gmail.com>
2022-03-18 17:10:12 -0300

    unio, intersection and difference

--- src/entity/Program.java
+++ src/entity/Program.java
@@ -1,27 +1,30 @@
 package entity;
 
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main (String [] args) {
 		
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10) );
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10) );
+		 
 		
-		Set<String> set = new LinkedHashSet<>();
+		//union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 		
-		set.add("Tv");
-		set.add("Tablet");
-		set.add("Notebook");
+		//intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
 		
+		//difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 		
-		set.removeIf(x -> x.charAt(0) == 'T');
-		
-	//	System.out.println(set.contains("Notebook"));
-		
-		
-		for (String p : set) {
-			System.out.println(p);
-		}
 	}
 }
